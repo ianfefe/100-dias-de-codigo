a5db5ade8dbeee660401938fa4bcf19dab9588f4
ianfefe <ianfelixfernandes@gmail.com>
2024-09-09 17:19:11 -0300

    dia5 upgrade

--- dia5/main.py
+++ dia5/main.py
@@ -1,26 +1,43 @@
-num1 = float(input("Digite o primeiro numero: "))
-num2 = float(input("Digite o segundo numero: "))
-print("1 - soma")
-print("2 - subtração")
-print("3 - divisão")
-print("4 - multiplicação")
-operacao = int(input("Escolha a operação que deseja fazer: "))
+def adicao(n1, n2):
+    return n1 + n2
 
-if(operacao == 1):
-    print("O resultado da soma é igual a: ")
-    print(num1 + num2)
+def subtracao(n1, n2):
+    return n1 - n2
 
-elif(operacao == 2):
-    print("O resultado da subtracao é igual a: ")
-    print(num1 - num2)
+def multiplicacao(n1, n2):
+    return n1 * n2
 
-elif(operacao == 3):
-    print("O resultado da divisao é igual a: ")
-    print(num1 / num2)
+def divisao(n1,n2):
+    return n1 / n2
 
-elif(operacao == 4):
-    print("O resultado da multiplicacao é igual a: ")
-    print(num1 * num2)
+def calculadora():
+    while True:
+        num1 = float(input("Digite o primeiro numero: "))
+        num2 = float(input("Digite o segundo numero: "))
+        print("1 - soma")
+        print("2 - subtração")
+        print("3 - divisão")
+        print("4 - multiplicação")
+        operacao = int(input("Escolha a operação que deseja fazer: "))
 
-else:
-    print("Operação inexistente")
+        if(operacao == 1):
+            print("O resultado da soma é igual a: ", adicao(num1,num2))
+
+        elif(operacao == 2):
+            print("O resultado da subtração é igual a: ", subtracao(num1,num2))
+
+        elif(operacao == 3):
+            print("O resultado da multiplicação é igual a: ", multiplicacao(num1,num2))
+
+        elif(operacao == 4):
+            print("O resultado da divisão é igual a: ", divisao(num1,num2))
+
+        else:
+            print("Operação inexistente")
+
+        continuar = input("Deseja fazer outra operação? (s/n): ")
+        if (continuar == 'n'):
+            print("Calculadora encerrada.")
+            break
+
+calculadora()

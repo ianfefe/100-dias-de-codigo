13bb3aa57ce0b5cc662c6d2f763c18e278f33bf2
ianfefe <ianfelixfernandes@gmail.com>
2024-09-10 16:42:12 -0300

    informa fatorial inexistente

--- dia6/main.py
+++ dia6/main.py
@@ -1,14 +1,13 @@
 def fatorial(n):
     if n <= 1:
         return 1
-
     else:
         return n * fatorial(n-1)
     
 while True:
 
     numero = int(input("Digite o número que deseja saber o fatorial: "))
-    print("O fatorial de ", numero, " é: ",fatorial(numero))
+    print("O fatorial de ", numero, " é: ", "Inexistente" if numero < 0 else fatorial(numero))
 
     continuar = input("Deseja fazer outra operação? (s/n): ")
     if (continuar == 'n'):

0877a1b8abae33c31a7c0ba6a24915cb68668767
ianfefe <ianfelixfernandes@gmail.com>
2024-09-18 00:47:15 -0300

    v3

--- dia-14/main.js
+++ dia-14/main.js
@@ -1,10 +1,14 @@
 function changeMode(mode) {
 
-    getTema = document.querySelector('.container')
+    getBotao = document.querySelector('.switcher')
+    getTema = document.body
     getTema.removeAttribute("data-theme")
-
-    if(mode == 0)
+    
+    if(mode == 0){
         getTema.setAttribute("data-theme", "light")
-    else
+        getBotao.setAttribute('onclick', "changeMode(1)")
+    }else{
         getTema.setAttribute("data-theme", "dark")
+        getBotao.setAttribute('onclick', "changeMode(0)")
+    }
 }
